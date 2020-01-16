79c078fd998b5769ab017ff338afdc60d1e39c28
Griffin Brookshire <brooksg1@regn.legal.net>
2020-01-16 16:01:57 -0500

    modified code to take input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println("Hello World!");
+        System.out.println("What would you like to say?");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.println("You said: " + input);
+        s.close();
     }
 }
