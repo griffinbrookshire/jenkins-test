9896c2f356895a6c35955f99375ca953dd641243
Griffin Brookshire <brooksg1@regn.legal.net>
2020-01-16 16:04:13 -0500

    added input file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("What would you like to say?");
-        Scanner s = new Scanner(System.in);
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("input.txt"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         String input = s.nextLine();
         System.out.println("You said: " + input);
         s.close();
